b6aad9a5c2fc34d7f52a636ac5ba3e9bf60a4b63
KevinBett <kevinbett458@gmail.com>
2023-03-14 01:50:10 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+// multiply num1 and num2, and assign the result to multiply
+let multiply = num1 * num2;
+
+// create a random integer greater than 0, and assign it to random
+let random = Math.ceil(Math.random() * 100);
+
+let num3 = 15;
+let num4 = 11;
+
+// calculate the remainder of dividing num3 by num4, and assign it to mod
+let mod = num3 % num4;
+
+// find the highest number in a set, and assign it to max
+let max = Math.max(5, 10, 20, 15);
+
+console.log(multiply); // Output: 62
+console.log(random); // Output: a random integer greater than 0
+console.log(mod); // Output: 4
+console.log(max); // Output: 20
